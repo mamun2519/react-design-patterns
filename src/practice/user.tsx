da4bc18d84bccd89642b86c6091b3194370c1762
mamun2519 <juborajislam46@gmail.com>
2025-03-09 19:48:59 +0600

    pratice: Higher Order pattern pratice

---
+++ src/practice/user.tsx
@@ -0,0 +1,14 @@
+type BaseResponse = {
+  data: unknown;
+  loading: boolean;
+  error: string | null;
+};
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
+
+export const Users = ({}) => {};
