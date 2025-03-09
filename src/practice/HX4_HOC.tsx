397e88361fa34776fbc8b6013604e39d4f01d541
mamun2519 <juborajislam46@gmail.com>
2025-03-09 19:44:45 +0600

    pratice: Higher Order pattern pratice

--- src/practice/HX4_HOC.tsx
+++ src/practice/HX4_HOC.tsx
@@ -1,5 +1,9 @@
-import { ComponentType } from "react";
+import { ComponentType, useState } from "react";
 
 const withDataFetch = (Component: ComponentType, url: string) => {
-  return (props: any) => {};
+  return (props: any) => {
+    const [data, setData] = useState<unknown>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+  };
 };
