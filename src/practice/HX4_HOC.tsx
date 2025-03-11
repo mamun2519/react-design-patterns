8902c393e204cbe702d27ae2527b777645a01048
mamun2519 <juborajislam46@gmail.com>
2025-03-11 22:33:36 +0600

    practice rendering props

--- src/practice/HX4_HOC.tsx
+++ src/practice/HX4_HOC.tsx
@@ -4,9 +4,3 @@ export const withComponentDataFetch = (
   Component: ComponentType | any,
   url: string
 ) => {
-  return (props: any) => {
-    const [data, setData] = useState<unknown>(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
-
-  
