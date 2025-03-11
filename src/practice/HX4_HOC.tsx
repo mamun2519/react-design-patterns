34acc05d1f99c1cab746a1cf1c24494496d70eb9
mamun2519 <juborajislam46@gmail.com>
2025-03-11 22:32:56 +0600

    practice rendering props

--- src/practice/HX4_HOC.tsx
+++ src/practice/HX4_HOC.tsx
@@ -42,7 +42,5 @@ export const withComponentDataFetch = (
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error}</p>;
-
-    return <Component data={data} {...props} />;
   };
 };
