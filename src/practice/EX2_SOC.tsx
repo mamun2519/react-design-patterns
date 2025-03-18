2494dee1fc4deda87bb59fcdef17dacedb22277f
mamun2519 <juborajislam46@gmail.com>
2025-03-18 21:10:21 +0600

    practice soc design pencipale

---
+++ src/practice/EX2_SOC.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function UserProfile({ userId }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/users/${userId}`)
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  }, [userId]);
+
+  if (!user) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>{user.email}</p>
+    </div>
+  );
+}
