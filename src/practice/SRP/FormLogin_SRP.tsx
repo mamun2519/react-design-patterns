ecaa33d77ad9694fc38a9aafa455a9cd2bf4dda5
mamun2519 <juborajislam46@gmail.com>
2025-03-18 22:13:22 +0600

    practice Separation of Concerns principle

---
+++ src/practice/SRP/FormLogin_SRP.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+//* Without SRP
+function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Email:", email, "Password:", password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
