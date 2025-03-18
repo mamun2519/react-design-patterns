35ecce55eddc76fe08cbbe8241081c54f11b7345
mamun2519 <juborajislam46@gmail.com>
2025-03-18 22:40:12 +0600

    practice Higher-Order Component (HOC)  principle

--- src/practice/HOC/Loading_HOC.tsx
+++ src/practice/HOC/Loading_HOC.tsx
@@ -16,3 +16,13 @@ const withLoading = (WrappedComment) => {
     return <WrappedComment {...props} />;
   };
 };
+
+// Regular Component
+const UserProfile = ({ user }) => {
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>{user.email}</p>
+    </div>
+  );
+};
