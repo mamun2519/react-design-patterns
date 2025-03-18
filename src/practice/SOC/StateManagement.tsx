8ce9fedca98f53f4d35fa29eafc7ca048aa7d05b
mamun2519 <juborajislam46@gmail.com>
2025-03-18 21:23:14 +0600

    practice Separation of Concerns principle

---
+++ src/practice/SOC/StateManagement.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+// Separating State Management (With SOC)
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
