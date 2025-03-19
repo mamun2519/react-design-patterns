8b748b8e2a43af809a967509149dbf7dc3e53e1c
mamun2519 <juborajislam46@gmail.com>
2025-03-19 21:13:15 +0600

    pratice  Compound Components patterns

---
+++ src/pattern practice/ compound Components/Tabs.tsx
@@ -0,0 +1,10 @@
+//create a context for sharing state
+
+import { createContext } from "react";
+
+const TabsContext = createContext();
+
+// Parent Component manage state
+const Tabs = ()=>{
+      return ()
+}
