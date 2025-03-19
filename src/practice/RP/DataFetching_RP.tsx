14b39e5c83a02eae004025b668e93b64dfbfb0be
mamun2519 <juborajislam46@gmail.com>
2025-03-19 19:27:30 +0600

    practice rendering props pattern

--- src/practice/RP/DataFetching_RP.tsx
+++ src/practice/RP/DataFetching_RP.tsx
@@ -1,5 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const UserDataFetching = ({ url, render }) => {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, [url]);
+
+  return render({ data, loading });
 };
