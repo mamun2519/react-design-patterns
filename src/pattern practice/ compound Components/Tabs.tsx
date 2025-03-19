0366260f6a16040e80ee78c2c509da0674843b27
mamun2519 <juborajislam46@gmail.com>
2025-03-19 21:14:31 +0600

    pratice  Compound Components patterns

--- src/pattern practice/ compound Components/Tabs.tsx
+++ src/pattern practice/ compound Components/Tabs.tsx
@@ -5,7 +5,7 @@ import { createContext, useState } from "react";
 const TabsContext = createContext(0);
 
 // Parent Component manage state
-const Tabs = ({children})=>{
-      const [activeIndex, setActiveIndex] = useState(0)
-      return ()
-}
+const Tabs = ({ children }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  return <TabsContext.Provider>{children}</TabsContext.Provider>;
+};
