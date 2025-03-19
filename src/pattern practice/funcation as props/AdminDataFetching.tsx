f93ecfdab264cd0a2072bbfc48fce2c7d9a591e5
mamun2519 <juborajislam46@gmail.com>
2025-03-19 19:58:33 +0600

    pactice to funcation as a props patterns

--- src/pattern practice/funcation as props/AdminDataFetching.tsx
+++ src/pattern practice/funcation as props/AdminDataFetching.tsx
@@ -1,3 +1,17 @@
-import { Children } from "react";
+import { Children, useEffect, useState } from "react";
 
-const AdminDataFetching = { url, children };
+const AdminDataFetching = ({ url, children }) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, [url]);
+
+  return children({ data, loading });
+};
