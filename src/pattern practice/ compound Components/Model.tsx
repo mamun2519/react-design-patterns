f42f18c4002d87336ae371d7566fc42e1ea10aa8
mamun2519 <juborajislam46@gmail.com>
2025-03-24 22:24:54 +0600

    added to compound components patterns

--- src/pattern practice/ compound Components/Model.tsx
+++ src/pattern practice/ compound Components/Model.tsx
@@ -1,5 +1,13 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const ModelContext = createContext();
 
-const Model = ({ children }) => {};
+const Model = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <ModelContext.Provider value={{ isOpen, setIsOpen }}>
+      {children}
+    </ModelContext.Provider>
+  );
+};
