e4f90b50d16dced2b9e6b37690eb33bb753727d6
mamun2519 <juborajislam46@gmail.com>
2025-03-24 23:10:10 +0600

    added to provider pattern

--- src/pattern practice/provider-pattern/ManageUser.tsx
+++ src/pattern practice/provider-pattern/ManageUser.tsx
@@ -1,5 +1,19 @@
 import { useState } from "react";
 
+// Without provider patten
+function App() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <div>
+      <Navbar user={user} setUser={setUser} />
+      <Dashboard user={user} />
+      <Settings user={user} />
+    </div>
+  );
+}
+
+// with provider pattens
 function App() {
   const [user, setUser] = useState(null);
 
