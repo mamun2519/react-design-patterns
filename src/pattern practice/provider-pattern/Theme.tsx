4b8e4efdbc25b29bc3cbea563e1bade599c8a019
mamun2519 <juborajislam46@gmail.com>
2025-03-24 23:05:39 +0600

    added to provider pattern

---
+++ src/pattern practice/provider-pattern/Theme.tsx
@@ -0,0 +1,30 @@
+function App() {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <div>
+      <Header theme={theme} setTheme={setTheme} />
+      <MainContent theme={theme} />
+      <Footer theme={theme} />
+    </div>
+  );
+}
+
+function Header({ theme, setTheme }) {
+  return (
+    <header style={{ background: theme === "light" ? "#fff" : "#333" }}>
+      <Button theme={theme} setTheme={setTheme} />
+    </header>
+  );
+}
+
+function Button({ theme, setTheme }) {
+  return (
+    <button
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      style={{ color: theme === "light" ? "#000" : "#fff" }}
+    >
+      Toggle Theme
+    </button>
+  );
+}
