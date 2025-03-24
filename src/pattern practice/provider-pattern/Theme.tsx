0dc3c08a4748f3e777a49c358b3f94fef941cb63
mamun2519 <juborajislam46@gmail.com>
2025-03-24 23:08:59 +0600

    added to provider pattern

--- src/pattern practice/provider-pattern/Theme.tsx
+++ src/pattern practice/provider-pattern/Theme.tsx
@@ -1,5 +1,38 @@
 import { useState } from "react";
 
+// Without provider patten
+function App() {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <div>
+      <Header theme={theme} setTheme={setTheme} />
+      <MainContent theme={theme} />
+      <Footer theme={theme} />
+    </div>
+  );
+}
+
+function Header({ theme, setTheme }) {
+  return (
+    <header style={{ background: theme === "light" ? "#fff" : "#333" }}>
+      <Button theme={theme} setTheme={setTheme} />
+    </header>
+  );
+}
+
+function Button({ theme, setTheme }) {
+  return (
+    <button
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      style={{ color: theme === "light" ? "#000" : "#fff" }}
+    >
+      Toggle Theme
+    </button>
+  );
+}
+
+// with provider props patten
 function App() {
   const [theme, setTheme] = useState("light");
 
